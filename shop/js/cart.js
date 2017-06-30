3fa2681205223c83dbef781cf0d6ec05298db2bd
qq2289633869 <2289633869@qq.com>
2017-06-30 10:13:50 +0800

    6.29

--- shop/js/cart.js
+++ shop/js/cart.js
@@ -1,3 +1,124 @@
-/**
- * Created by 22896 on 2017/6/28.
- */
+$(function () {
+    console.log(123);
+    var shopping = $("#shopping");
+    $.ajax({
+        "url": "http://h6.duchengjiu.top/shop/api_cart.php",
+        "type":"GET",
+        "dataType":"json",
+        "data":{
+          "token":localStorage.token
+        },
+        "success":function (response) {
+            console.log(response);
+            for(var i = 0; i < response.data.length; i++){
+                var obj = response.data[i];
+                shopping.append("<tr class='tr-goods tr-goods"+ obj.goods_id+"'>" +
+                    " <td class='choice'><input type='checkbox' checked='checked' class='choiceInput' ></td>" +
+                    " <td class='commodity'><a href='details.html?goods_id=" + obj.goods_id+ "' >" +
+                    "<img src='" + obj.goods_thumb + "' alt=''><h1>" + obj.goods_name + "</h1></a></td> " +
+                    "<td class='commodity-num'><button class='numLeft'>-</button>" +
+                    "<input type='tel' value='" +obj.goods_number+ "'  class='goodsNmu numNmu" + obj.goods_id + "'>" +
+                    "<button class='numRight'>+</button></td><td class='univalence'>" + obj.goods_price + "元</td> " +
+                    "<td class='subtotal'>" +(obj.goods_number*obj.goods_price)+ "元</td> <td class='operation'>" +
+                    "<button class='del'>删除</button></td><input type='hidden' value='"+ obj.goods_id +"'></tr>");
+            }
+            totalPrice();
+            function totalPrice() {
+                var sum = 0;
+                var totalPrice = $(":checked").parent().nextAll(".subtotal");
+                console.log(totalPrice);
+                for(var i = 0; i < totalPrice.length; i++){
+                    sum += parseInt (totalPrice[i].innerText);
+                }
+                $("#totalPrice").text("总价：￥"+sum);
+            }
+            WholeChoice();
+            function WholeChoice() {
+                var sum = 0;
+                for(var i = 0; i < $(".choiceInput").length; i++) {
+                    if ($(".choiceInput")[i].checked == true) {
+                        sum +=1;
+                    }
+                    if(sum == $(".choiceInput").length){
+                        $("#wholeChoice")[0].checked = true;
+                    }else{
+                        $("#wholeChoice")[0].checked = false;
+                    }
+                }
+            }
+            function Del(goodsId) {
+                $.ajax({
+                    "url":"http://h6.duchengjiu.top/shop/api_cart.php?token=" + localStorage.getItem("token"),
+                    "type":"POST",
+                    "dataType":"json",
+                    "data":{
+                        "goods_id":goodsId,
+                        "number": 0
+                    },
+                    "success":function () {
+
+                    }
+                });
+            }
+            shopping[0].addEventListener("click",function (event) {
+               event = event || window.event;
+               var target = event.target || event.srcElement;
+               if(target.id == "wholeChoice"){
+                       var Checked = wholeChoice.checked;
+                       console.log(Checked);
+                       for(var i = 0; i < $(".choiceInput").length; i++){
+                           $(".choiceInput")[i].checked = Checked;
+                       }
+                       totalPrice();
+               }
+               if(target.className == "choiceInput"){
+                   totalPrice();
+                   WholeChoice();
+               }
+               if(target.className == "numLeft"){
+                   var Next =  $(event.target).next();
+                   var sum =parseInt(Next.val());
+                   if(sum === 1){
+                       return;
+                   }
+                   Next.val(sum-1);
+                   var univalence = $(event.target).parent().next();
+                   var subtotal = $(event.target).parent().next().next();
+                   subtotal.text(parseInt(univalence.text())*parseInt(Next.val())+"元");
+                   totalPrice();
+               }
+                if(target.className == "numRight"){
+                    var Prev =  $(event.target).prev();
+                    var sum =parseInt(Prev.val());
+                    if(sum === 10){
+                        return;
+                    }
+                    Prev.val(sum+1);
+                    var univalence = $(event.target).parent().next();
+                    var subtotal = $(event.target).parent().next().next();
+                    subtotal.text(parseInt(univalence.text())*parseInt(Prev.val())+"元");
+                    totalPrice();
+                }
+                if(target.className == "del"){
+                    var goodsId = $(event.target).parent().next().val();
+                    console.log(goodsId);
+                    Del(goodsId);
+                    $(event.target).parent().parent().remove();
+                }
+            });
+            $("#batch").click(function () {
+                var checkedNum = $(".choiceInput");
+                for(var i = 0; i < checkedNum.length; i++) {
+                    console.log($(".choiceInput").length);
+                    if (checkedNum[i].checked == true) {
+                        var Node = checkedNum[i].parentNode.parentNode;
+                        var NodeVal = $(Node).find("input:hidden").val();
+                        console.log(NodeVal);
+                        Del(NodeVal);
+                        $(Node).remove();
+                    }
+                }
+            })
+        }
+    })
+});
--- shop/js/details.js
+++ shop/js/details.js
@@ -14,7 +14,6 @@ $(function () {
                 + obj.goods_name +"</h1><b>￥"+ obj.price
                 +" </b><p>" + obj.goods_desc
                 + "</p><div id='goodsNumber' class='goodsNumber'><button class='numLeft' id='numLeft'>-</button><input type='tel' class='goodsNmu' id='goodsNmu' min='1' max='10' value='1'><button class='numRight' id='numRight'>+</button></div><br><a href='#'>立即购买</a><a id='purchase'>加入购物车</a>")
-            var sum = 1;
             var numLeft = $("#numLeft");
             var goodsNmu = $("#goodsNmu");
             var numRight = $("#numRight");
@@ -32,30 +31,52 @@ $(function () {
                 }
             });
             numLeft.click(function () {
-                sum--;
-                sum = sum < 1 ? 1 : sum;
-                goodsNmu.val(sum);
+                // sum--;
+                // sum = sum < 1 ? 1 : sum;
+                if(goodsNmu.val() == 1){
+                    return;
+                }
+                goodsNmu.val(parseInt(goodsNmu.val())-1);
             });
             numRight.click(function () {
-                sum++;
-                sum = sum > 10 ? 10 : sum;
-                goodsNmu.val(sum);
+                // sum++;
+                // sum = sum > 10 ? 10 : sum;
+                if(goodsNmu.val() == 10){
+                    return;
+                }
+                goodsNmu.val(parseInt(goodsNmu.val())+1);
             });
+            var number = 0;
+
             $("#purchase").click(function () {
+                number += parseInt(goodsNmu.val());
+                if(number > 10) number = 10;
+                if(number < 1) number = 1;
                 if(localStorage.getItem("token") == null){
                     Login();
                 }else{
+                    console.log(number);
+                    if(localStorage.getItem(goods_id+"_num",number) == 10 && localStorage.getItem("cart_id"+goods_id,goods_id)){
+                        alert("达到上限");
+                        return;
+                    }
+                    localStorage.setItem(goods_id+"_num",number);
+                    localStorage.setItem("cart_id"+goods_id,goods_id);
                     $.ajax({
                         "url":"http://h6.duchengjiu.top/shop/api_cart.php?token="+localStorage.token,
                         "type":"post",
                         "dataType":"json",
                         "data":{
                             "goods_id": goods_id,
-                            "number": sum
+                            "number": localStorage.getItem(goods_id+"_num")
                         },
                         "success":function (response) {
                             console.log(response);
-                            alert(response.message);
+                            if(response.code == 0){
+                                alert(response.message)
+                            }else if(response.code == 2){
+                                alert("只能购买10个");
+                            }
                         }
                     })
                 }
